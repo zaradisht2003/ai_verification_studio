ecc4d88077c420a364c2a2052ee7a1894a580db7
bolawarsy <bola.yousry@aallie.com>
2026-02-26 20:37:34 +0200

    Removed corrupted build directory

--- build_corrupted/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ai_verification_studio_FILE C:/Users/Dell/.gemini/antigravity/scratch/ai_verification_studio/build/ai_verification_studio.exe)
-set(__QT_DEPLOY_TARGET_ai_verification_studio_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ai_verification_studio_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
